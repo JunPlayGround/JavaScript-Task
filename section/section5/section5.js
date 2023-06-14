2006af56182771accfdc0b40117a84ce52a0189b
JunPlayGround <132260289+JunPlayGround@users.noreply.github.com>
2023-06-15 05:03:24 +0900

    ローカルと同期

--- section/section5/section5.js
+++ section/section5/section5.js
@@ -1,3 +1,9 @@
-const score = null;
+const score = function(a){
+    if(80<=a && a<=100) return "合格";
+    else if(60<=a && a<80) return "追試";
+    else if(0<=a && a<60) return "不合格";
+    else if(a === undefined) return "後日受験";
+    else return "0以上100未満の数字を入れてください"
+};
 
 module.exports = { score };
